986e82611f00f9cc8be46a36421ccbb9fcd81a72
alwaystest18 <hrayha@163.com>
2023-04-23 14:19:21 +0800

    优化程序内存占用

--- cdnChecker.go
+++ cdnChecker.go
@@ -1,33 +1,31 @@
 package main
 
 import (
-	"os"
-	"fmt"	
 	"flag"
-	"strings"
+	"fmt"
 	"io/ioutil"
 	"math"
-	"sync"
 	"math/rand"
-	"time"
+	"os"
 	"sort"
+	"strings"
+	"sync"
+	"time"
 
-	"github.com/projectdiscovery/retryabledns"
-	"github.com/projectdiscovery/dnsx/libs/dnsx"
-	"github.com/miekg/dns"
 	"github.com/Workiva/go-datastructures/queue"
+	"github.com/miekg/dns"
+	"github.com/projectdiscovery/dnsx/libs/dnsx"
+	"github.com/projectdiscovery/retryabledns"
 )
 
-
-var validResolversList []string   //可用dns服务器列表
-var noCdnDomains []string      //未使用cdn的域名列表
+var validResolversList []string //可用dns服务器列表
+var noCdnDomains []string       //未使用cdn的域名列表
 var useCdnDomains []string      //使用cdn的域名列表
-var noCdnIps []string         //未使用cdn的ip列表
-var domainsInfo []string  //所有域名+对应ip信息记录在此列表中
+var noCdnIps []string           //未使用cdn的ip列表
+var domainsInfo []string        //所有域名+对应ip信息记录在此列表中
 var wg sync.WaitGroup
 
-
-//将文件内容转为字符列表，主要用于域名列表、cdn cname列表与dns服务器
+// 将文件内容转为字符列表，主要用于域名列表、cdn cname列表与dns服务器
 func FileContentToList(filePath string) []string {
 	fileContent, err := ioutil.ReadFile(filePath)
 	if err != nil {
@@ -38,18 +36,17 @@ func FileContentToList(filePath string) []string {
 	return contentList
 }
 
-//判断字符串在数组元素中是否包含
+// 判断字符串在数组元素中是否包含
 func In(target string, str_array []string) bool {
-    sort.Strings(str_array)
-    index := sort.SearchStrings(str_array, target)
-    if index < len(str_array) && str_array[index] == target {
-        return true
-    }
-    return false
+	sort.Strings(str_array)
+	index := sort.SearchStrings(str_array, target)
+	if index < len(str_array) && str_array[index] == target {
+		return true
+	}
+	return false
 }
 
-
-//通过解析特定域名获取ip地址来找出提供列表中可用dns服务器
+// 通过解析特定域名获取ip地址来找出提供列表中可用dns服务器
 func FilterValidResolver(resolver string) {
 	defer wg.Done()
 	retries := 1
@@ -61,7 +58,7 @@ func FilterValidResolver(resolver string) {
 	}
 }
 
-//判断域名的cname是否包含cdn cname，如果包含则判定使用了cdn
+// 判断域名的cname是否包含cdn cname，如果包含则判定使用了cdn
 func InCdnCnameList(domainCnameList []string, cdnCnameList []string) bool {
 	inCdnCname := false
 	for _, domainCname := range domainCnameList {
@@ -75,8 +72,7 @@ func InCdnCnameList(domainCnameList []string, cdnCnameList []string) bool {
 	return inCdnCname
 }
 
-
-//获取域名在特定dns上的解析ip，并且以.分割ip，取ip的前三部分，即解析ip为1.1.1.1,最终输出为[1.1.1],便于判断多个ip是否在相同网段
+// 获取域名在特定dns上的解析ip，并且以.分割ip，取ip的前三部分，即解析ip为1.1.1.1,最终输出为[1.1.1],便于判断多个ip是否在相同网段
 func ResolvDomainIpPart(domain string, resolver string) ([]string, error) {
 	var domainIpsPart []string
 	retries := 1
@@ -89,31 +85,31 @@ func ResolvDomainIpPart(domain string, resolver string) ([]string, error) {
 	if err != nil {
 		return domainIpsPart, err
 	}
-	ipsList := dnsResponses.A       //[1.1.1.1, 2.2.2.2, 3.3.3.3]
+	ipsList := dnsResponses.A //[1.1.1.1, 2.2.2.2, 3.3.3.3]
 	if len(ipsList) > 0 {
 		for _, ip := range ipsList {
 			ipParts := strings.Split(ip, ".")
 			ipSplit := ipParts[0] + "." + ipParts[1] + "." + ipParts[2]
-			domainIpsPart = append(domainIpsPart, ipSplit)
+			domainIpsPart = UniqueStrList(append(domainIpsPart, ipSplit))
 		}
 	}
 	return domainIpsPart, nil
 }
 
-//字符型列表去重去空
+// 字符型列表去重去空
 func UniqueStrList(strList []string) []string {
-    uniqList := make([]string, 0)
-    tempMap := make(map[string]bool, len(strList))
-    for _, v := range strList {
-        if tempMap[v] == false && len(v) > 0 {
-            tempMap[v] = true
-            uniqList = append(uniqList, v)
-        }
-    }
-    return uniqList
+	uniqList := make([]string, 0)
+	tempMap := make(map[string]bool, len(strList))
+	for _, v := range strList {
+		if tempMap[v] == false && len(v) > 0 {
+			tempMap[v] = true
+			uniqList = append(uniqList, v)
+		}
+	}
+	return uniqList
 }
 
-//生成count个[start,end)结束的不重复的随机数
+// 生成count个[start,end)结束的不重复的随机数
 func GenerateRandomNumber(start int, end int, count int) []int {
 	//范围检查
 	if end < start || (end-start) < count {
@@ -145,41 +141,28 @@ func GenerateRandomNumber(start int, end int, count int) []int {
 	return nums
 }
 
-
-//判断域名是否使用cdn
-func CdnCheck(domain string, cdnCnameList []string, resolversList []string) {
+// 判断域名是否使用cdn
+func CdnCheck(domain string, cdnCnameList []string, resolversList []string, dnsxClient *dnsx.DNSX) {
 	defer wg.Done()
 
-	//dnsx库初始化配置
-    var DefaultOptions = dnsx.Options{
-    	BaseResolvers:     resolversList,
-    	MaxRetries:        5,
-    	QuestionTypes:     []uint16{dns.TypeA},
-    	TraceMaxRecursion: math.MaxUint16,
-    	Hostsfile:         true,
-    }
-
-	//init dnsx client
-    dnsxClient, _ := dnsx.New(DefaultOptions)
-
-    //使用dnsx库自带的cdn判断函数检测
-	useCdnDnsx, _, _ := dnsxClient.CdnCheck(domain)
-	if useCdnDnsx {
-		useCdnDomains = append(useCdnDomains, domain)
-	}
-    //get domain cnames and ips
+	isCdn, _, _ := dnsxClient.CdnCheck(domain) //通过dnsx自带方法识别cdn，主要为根据ip范围判断国外主流cdn厂商
 	dnsxResult, _ := dnsxClient.QueryOne(domain)
 	domainCnameList := dnsxResult.CNAME
 	domainIpList := dnsxResult.A
-	for _, domainIp := range domainIpList {
-		domainsInfo = append(domainsInfo, domain + ":" + domainIp)
+
+	if isCdn {
+		useCdnDomains = append(useCdnDomains, domain)
 	}
 
-	if len(domainCnameList) == 0 && len(domainIpList) > 0 {   //无cname但有A记录，直接判定未使用cdn
+	for _, domainIp := range domainIpList {
+		domainsInfo = UniqueStrList(append(domainsInfo, domain+":"+domainIp))
+	}
+
+	if len(domainCnameList) == 0 && len(domainIpList) > 0 { //无cname但有A记录，直接判定未使用cdn
 		noCdnDomains = append(noCdnDomains, domain)
-		noCdnIps = append(noCdnIps, domainIpList...)
+		noCdnIps = UniqueStrList(append(noCdnIps, domainIpList...))
 	} else if len(domainCnameList) > 0 && len(domainIpList) > 0 {
-		if InCdnCnameList(domainCnameList, cdnCnameList) {   //cdn在cdn cname列表中包含，直接判定使用cdn
+		if InCdnCnameList(domainCnameList, cdnCnameList) { //cdn在cdn cname列表中包含，直接判定使用cdn
 			useCdnDomains = append(useCdnDomains, domain)
 		} else {
 			var domainIpPartList []string
@@ -190,33 +173,32 @@ func CdnCheck(domain string, cdnCnameList []string, resolversList []string) {
 				if err != nil {
 					continue
 				}
-				domainIpPartList = append(domainIpPartList, domainIpsWithResolver...)
-				if len(UniqueStrList(domainIpPartList)) > 3 {   //不同段ip数量达到4个就跳出循环，避免每个dns服务器都解析增加耗时
+				domainIpPartList = UniqueStrList(append(domainIpPartList, domainIpsWithResolver...))
+				if len(domainIpPartList) > 3 { //不同段ip数量达到4个就跳出循环，避免每个dns服务器都解析增加耗时
 					break
-				} 
+				}
 			}
 			//不同段ip数量达到4个就判定为使用了cdn
-			if len(UniqueStrList(domainIpPartList)) > 3 {
+			if len(domainIpPartList) > 3 {
 				useCdnDomains = append(useCdnDomains, domain)
 			} else {
 				noCdnDomains = append(noCdnDomains, domain)
-				noCdnIps = append(noCdnIps, domainIpList...)
+				noCdnIps = UniqueStrList(append(noCdnIps, domainIpList...))
 			}
 		}
 	}
 }
 
-
 func main() {
 	nowTime := time.Now().Format("200601021504")
-    df := flag.String("df", "", "domain list file")
+	df := flag.String("df", "", "domain list file")
 	cf := flag.String("cf", "cdn_cname", "cdn cname file")
 	r := flag.String("r", "", "dns resolvers file")
-	o := flag.String("o", "no_cdn_domains" + nowTime + ".txt", "output domains that are not using cdn to file")
-	oi := flag.String("oi", "no_cdn_ips" + nowTime + ".txt", "output ips that are not using cdn to file")
-	oc := flag.String("oc", "use_cdn_domains" + nowTime + ".txt", "output domains that are using cdn to file")
-	od := flag.String("od", "domain_info" + nowTime + ".txt", "output domain info(domain:ip) to file")
-	flag.Parse() 
+	o := flag.String("o", "no_cdn_domains"+nowTime+".txt", "output domains that are not using cdn to file")
+	oi := flag.String("oi", "no_cdn_ips"+nowTime+".txt", "output ips that are not using cdn to file")
+	oc := flag.String("oc", "use_cdn_domains"+nowTime+".txt", "output domains that are using cdn to file")
+	od := flag.String("od", "domain_info"+nowTime+".txt", "output domain info(domain:ip) to file")
+	flag.Parse()
 
 	noCdnDomainsFileName := *o
 	noCdnIpsFileName := *oi
@@ -237,7 +219,7 @@ func main() {
 	resolversFile := *r
 	tempResolversList := UniqueStrList(FileContentToList(resolversFile))
 
-	queResolvers := queue.New(5)   //dns服务器队列
+	queResolvers := queue.New(5) //dns服务器队列
 	for _, resolver := range tempResolversList {
 		queResolvers.Put(resolver)
 	}
@@ -249,15 +231,24 @@ func main() {
 	}
 	wg.Wait()
 	if len(validResolversList) < 20 {
-        fmt.Println("The number of valid resolvers is less than 20, please improve it: " + resolversFile)
+		fmt.Println("The number of valid resolvers is less than 20, please improve it: " + resolversFile)
 		os.Exit(2)
 	}
 
+	var DefaultOptions = dnsx.Options{
+		BaseResolvers:     validResolversList,
+		MaxRetries:        5,
+		QuestionTypes:     []uint16{dns.TypeA},
+		TraceMaxRecursion: math.MaxUint16,
+		Hostsfile:         true,
+	}
+	//init dnsx client
+	dnsxClient, _ := dnsx.New(DefaultOptions)
 
-	que := queue.New(5)   //域名队列
+	que := queue.New(5) //域名队列
 
-    for _, domain := range domainsList {
-        que.Put(domain)
+	for _, domain := range domainsList {
+		que.Put(domain)
 	}
 
 	for que.Len() > 0 {
@@ -265,8 +256,7 @@ func main() {
 		queDomainList, _ := que.Get(1)
 		queDomain := queDomainList[0].(string)
 
-
-		go CdnCheck(queDomain, cdnCnameList, validResolversList)
+		go CdnCheck(queDomain, cdnCnameList, validResolversList, dnsxClient)
 	}
 	wg.Wait()
 
@@ -290,7 +280,6 @@ func main() {
 			noCdnIpsFile.WriteString(noCdnIp + "\n")
 		}
 	}
-
 
 	//使用cdn域名写入文件
 	if len(useCdnDomains) > 0 {
